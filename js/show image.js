76ee96e78f024e75bca909f8fd920030828b175d
Shivam Padmani <56795691+shivampadmani@users.noreply.github.com>
2020-10-19 20:10:16 +0530

    added js folder only

---
+++ js/show image.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+
+function swapImage(){
+	var image = document.getElementById("imageToSwap");
+	var dropd = document.getElementById("dlist");
+	image.src = dropd.value;	
+};
+</script>
